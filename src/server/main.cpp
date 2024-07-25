ced8ca56de737af37e89877c537ccfe0becbc8f8
roxlong <1432158320@qq.com>
2024-07-25 20:38:10 +0800

    all code and readme

---
+++ src/server/main.cpp
@@ -0,0 +1,36 @@
+#include "chatserver.hpp"
+#include "chatservice.hpp"
+#include <iostream>
+#include <signal.h>
+using namespace std;
+
+
+void resetHandler(int)
+{
+    ChatService::instance()->reset();
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        cerr << "command invalid! example: ./ChatServer 192.168.239.128" << endl;
+        exit(-1);
+    }
+
+    // 解析通过命令行参数传递的ip和port
+    char *ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    signal(SIGINT, resetHandler);
+
+    EventLoop loop;
+    InetAddress addr(ip, port);
+    ChatServer server(&loop, addr, "ChatServer");
+
+    server.start();
+    loop.loop();
+
+    return 0;
+}
